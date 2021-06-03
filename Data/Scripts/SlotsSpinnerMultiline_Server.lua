2276596fde0e3404bf8258a37b17d02fcee1927a
beneilts <36256880+beneilts@users.noreply.github.com>
2021-06-03 13:21:03 -0700

    Fixed the win line animation

--- Data/Scripts/SlotsSpinnerMultiline_Server.lua
+++ Data/Scripts/SlotsSpinnerMultiline_Server.lua
@@ -146,6 +146,17 @@ function PickItemRandomly(player, betAmount, slotId)
     for i = 1, 9 do
         slotsTable[i] = GetRandomSlot(reelTotal)
     end
+    
+    --[[
+    slotsTable[1] = 3
+    slotsTable[2] = testValue
+    slotsTable[3] = 3
+    slotsTable[4] = 4
+    slotsTable[5] = testValue
+    slotsTable[6] = 4
+    slotsTable[7] = 3
+    slotsTable[8] = testValue
+    slotsTable[9] = 3]]
 
     local dataStr = API.ConvertTableToString(slotsTable)
     newData:SetNetworkedCustomProperty("data", dataStr)
